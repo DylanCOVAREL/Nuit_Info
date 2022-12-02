9f68365ea6b965f8101f72a7f1cd89d09011a883
Rhizlene <rhizlene.alfardous@gmail.com>
2022-12-02 05:42:18 +0100

    bdd2

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+
+/**
+ * @author nuit info
+ * @version 1.0.0
+ * @description Modèle d'application MVC utisant Mongoose, Express, EJS... et un peu de JSDoc
+ * La source originale du code est accessible sur https://www.c-sharpcorner.com/blogs/how-to-setup-mvc-design-pattern-in-express
+ * Toutefois, le code a été fortement remanié sur certains aspects (pas de callback sur la partie modèle).
+ */
+
 const routeur = require('./routes/route.js');
 const express = require('express');
 const ejs = require('ejs');
+const path     = require('path')  
 
 
 // activer les dépendances pour Express et EJS
@@ -9,6 +19,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('views'))
 app.use(express.static('public'))
 
+app.set("views",path.resolve(__dirname,'views'))
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 
